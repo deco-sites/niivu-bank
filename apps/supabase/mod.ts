9706d647632c632c6868fbdca7a18699f74305e7
Gabriel Matos Boubee <108771428+gabrielMatosBoubee@users.noreply.github.com>
2024-02-19 20:37:45 -0300

    feat: add supabase's client

---
+++ apps/supabase/mod.ts
@@ -0,0 +1,25 @@
+import {
+  createClient,
+  SupabaseClient,
+} from "https://esm.sh/v135/@supabase/supabase-js@2.7.0";
+import { Secret } from "apps/website/loaders/secret.ts";
+import manifest, { Manifest } from "./manifest.gen.ts";
+import { App } from "deco/mod.ts";
+
+export interface Props {
+  url: string;
+  token: Secret;
+}
+
+export interface State extends Props {
+  api: SupabaseClient;
+}
+
+export default function App({ url, token }: Props): App<Manifest, State> {
+  const stringToken = typeof token === "string" ? token : token?.get?.() ?? "";
+  const api = createClient(url, stringToken);
+  return {
+    manifest,
+    state: { url, token, api },
+  };
+}

7bf782b0bf57a433193febfd8133174519dbec49
Jonas Jesus <jonasdasilvajesus@outlook.com>
2024-02-21 22:56:37 -0300

    fix: modifying the supabase client

--- apps/supabase/mod.ts
+++ apps/supabase/mod.ts
@@ -3,8 +3,7 @@ import {
   SupabaseClient,
 } from "https://esm.sh/v135/@supabase/supabase-js@2.7.0";
 import { Secret } from "apps/website/loaders/secret.ts";
-import manifest, { Manifest } from "./manifest.gen.ts";
-import { App } from "deco/mod.ts";
+import { supabase } from "deco/deps.ts";
 
 export interface Props {
   token?: Secret;
@@ -15,11 +14,25 @@ export interface State extends Props {
   supabase: SupabaseClient;
 }
 
-export default function App({ url, token}: Props): App<Manifest, State> {
-  const stringToken = typeof token === "string" ? token : token?.get?.() ?? "";
-  const supabase = createClient(url, stringToken);
-  return {
-    manifest,
-    state: { url, token, supabase },
-  };
+let client: supabase.SupabaseClient | null = null;
+const DEFAULT_SUPABASE_LIVE_ENDPOINT =
+  "https://jtqltxymgxpncsfhwcwd.supabase.co/";
+
+const DEFAULT_SUPABASE_LIVE_ANON_KEY =
+  "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Imp0cWx0eHltZ3hwbmNzZmh3Y3dkIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MDgyMTQ4OTEsImV4cCI6MjAyMzc5MDg5MX0.aTbbQYCCmZOuiid9fr36_WrirTiyqa2s4TKwxdPwQt8";
+
+let userEndpoint = DEFAULT_SUPABASE_LIVE_ENDPOINT;
+let userKey = DEFAULT_SUPABASE_LIVE_ANON_KEY;
+
+export function setupSupabase(endpoint: string, key: string) {
+  userEndpoint = endpoint;
+  userKey = key;
+}
+
+export default function getSupabaseClient() {
+  if (!client) {
+    client = createClient(userEndpoint, userKey);
+  }
+
+  return client;
 }

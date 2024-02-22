74205adeed4d6bc3f666ca7911ff23f40b2ec7fc
Jonas Jesus <jonasdasilvajesus@outlook.com>
2024-02-21 22:10:43 -0300

    fix: changing API name to supabase

--- apps/supabase/mod.ts
+++ apps/supabase/mod.ts
@@ -7,19 +7,19 @@ import manifest, { Manifest } from "./manifest.gen.ts";
 import { App } from "deco/mod.ts";
 
 export interface Props {
+  token?: Secret;
   url: string;
-  token: Secret;
 }
 
 export interface State extends Props {
-  api: SupabaseClient;
+  supabase: SupabaseClient;
 }
 
-export default function App({ url, token = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Imp0cWx0eHltZ3hwbmNzZmh3Y3dkIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MDgyMTQ4OTEsImV4cCI6MjAyMzc5MDg5MX0.aTbbQYCCmZOuiid9fr36_WrirTiyqa2s4TKwxdPwQt8" as unknown as Secret}: Props): App<Manifest, State> {
+export default function App({ url, token}: Props): App<Manifest, State> {
   const stringToken = typeof token === "string" ? token : token?.get?.() ?? "";
-  const api = createClient(url, stringToken);
+  const supabase = createClient(url, stringToken);
   return {
     manifest,
-    state: { url, token, api },
+    state: { url, token, supabase },
   };
 }

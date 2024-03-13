00cddad7a1eac6c8dbb1f2b06c76bf3d2e4755b5
Jonas Jesus <jonasdasilvajesus@outlook.com>
2024-03-12 22:44:56 -0300

    fix: add route

--- routes/webhook/sendEmail.ts
+++ routes/webhook/sendEmail.ts
@@ -1,5 +1,8 @@
 export async function handler(req: unknown, ctx: unknown){
     console.log( req.body );
+    const t = req.json()
+    console.log({ t });
+    
     console.log({ ctx });
     console.log();
     

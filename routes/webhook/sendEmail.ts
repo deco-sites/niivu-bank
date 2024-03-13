04bd4e5670973e7e311dde9af10da9eb635f3717
Jonas Jesus <jonasdasilvajesus@outlook.com>
2024-03-12 23:01:29 -0300

    fix: add route

--- routes/webhook/sendEmail.ts
+++ routes/webhook/sendEmail.ts
@@ -1,11 +1,12 @@
 import { AppContext } from "deco-sites/niivu-bank/apps/site.ts";
 
 export async function handler(req: Request, ctx: AppContext, ...args: unknown[]){
-    // Lê o corpo da solicitação como texto
     const bodyText = await req.text();
     console.log("Corpo da solicitação:", bodyText);
     
-    // Passa o corpo da solicitação como argumento para a função invocada
+    const requestBody = JSON.parse(bodyText);
+    console.log("Corpo da solicitação (JSON):", requestBody);
+    
     return await ctx.state.invoke("deco-sites/niivu-bank/loaders/actions/sendEmail.ts", {
         reqBody: bodyText,
     });

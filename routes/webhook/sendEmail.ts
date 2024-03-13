e267c7603b1bc45fa569f9aac2c8c26b4a8ce52d
Jonas Jesus <jonasdasilvajesus@outlook.com>
2024-03-12 21:09:54 -0300

    fix: add route

--- routes/webhook/sendEmail.ts
+++ routes/webhook/sendEmail.ts
@@ -1,5 +1,6 @@
 export async function handler(req: unknown, ctx: unknown){
-    console.log(req);
+    console.log({ req });
+    console.log({ ctx });
     
     return await ctx.state.invoke("deco-sites/niivu-bank/loaders/actions/sendEmail.ts", {
         req: req,

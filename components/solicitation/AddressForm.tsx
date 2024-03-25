3e9275a53b180806b982c4dc4eaeef72d0ec4e3c
Gabriel Matos Boubee <108771428+gabrielMatosBoubee@users.noreply.github.com>
2024-03-25 18:42:50 -0300

    feat: add standard input

--- components/solicitation/AddressForm.tsx
+++ components/solicitation/AddressForm.tsx
@@ -1,4 +1,5 @@
-import { Cep } from "$store/islands/Cep.tsx"
+import { Cep } from "$store/islands/Cep.tsx";
+import StandardInput from "$store/components/ui/inputs/Standard.tsx";
 
 function AddressForm() {
   const divider = <div class="w-full h-[1px] bg-black bg-opacity-40" />;
@@ -11,79 +12,44 @@ function AddressForm() {
         <div class="flex flex-col lg:flex-row gap-2 flex-grow">
           <Cep />
 
-          <div class="flex flex-col gap-1 flex-grow">
-            <label class="text-primary font-medium" htmlFor="street">
-              <span>Rua / Avenida</span>
-            </label>
-            <input
-              class="input border border-black rounded-t-lg"
-              type="text"
-              id="street"
-              name="street"
-              placeholder="Digite Sua Rua Aqui"
-              required
-            />
-          </div>
+          <StandardInput
+            labelText="Rua / Avenida"
+            id="street"
+            placeholder="Digite Sua Rua Aqui"
+            required
+          />
         </div>
 
         <div class="flex flex-col lg:flex-row gap-2 flex-grow">
-          <div class="flex flex-col gap-1 flex-grow">
-            <label class="text-primary font-medium" htmlFor="number">
-              <span>Número</span>
-            </label>
-            <input
-              class="input border border-black rounded-t-lg"
-              type="text"
-              id="number"
-              name="number"
-              placeholder="xxx"
-              required
-            />
-          </div>
+          <StandardInput
+            labelText="Número"
+            id="number"
+            placeholder="xxx"
+            required
+          />
 
-          <div class="flex flex-col gap-1 flex-grow">
-            <label class="text-primary font-medium" htmlFor="complement">
-              <span>Complemento</span>
-            </label>
-            <input
-              class="input border border-black rounded-t-lg"
-              type="text"
-              id="complement"
-              name="complement"
-              placeholder="Digite Seu Complemento Aqui"
-              required
-            />
-          </div>
+          <StandardInput
+            labelText="Complemento"
+            id="complement"
+            placeholder="Digite Seu Complemento Aqui"
+            required
+          />
         </div>
 
         <div class="flex flex-col lg:flex-row gap-2 flex-grow">
-          <div class="flex flex-col gap-1 flex-grow">
-            <label class="text-primary font-medium" htmlFor="city">
-              <span>Cidade</span>
-            </label>
-            <input
-              class="input border border-black rounded-t-lg"
-              type="text"
-              id="city"
-              name="city"
-              placeholder="Digite Sua Cidade Aqui"
-              required
-            />
-          </div>
+          <StandardInput
+            labelText="Cidade"
+            id="city"
+            placeholder="Digite Sua Cidade Aqui"
+            required
+          />
 
-          <div class="flex flex-col gap-1 flex-grow">
-            <label class="text-primary font-medium" htmlFor="state">
-              <span>Estado</span>
-            </label>
-            <input
-              class="input border border-black rounded-t-lg"
-              type="text"
-              id="state"
-              name="state"
-              placeholder="Digite Seu Estado aqui"
-              required
-            />
-          </div>
+          <StandardInput
+            labelText="Estado"
+            id="state"
+            placeholder="Digite Seu Estado aqui"
+            required
+          />
         </div>
       </div>
     </>
---
+++ components/ui/inputs/Standard.tsx
@@ -0,0 +1,30 @@
+export interface Props {
+  labelText: string;
+  id: string;
+  name?: string;
+  placeholder?: string;
+  required?: boolean;
+  type?: string;
+}
+
+function Standard(
+  { labelText, id, name, placeholder, type = "text", required }: Props,
+) {
+  return (
+    <div class="flex flex-col gap-1 flex-grow">
+      <label class="text-primary font-medium" htmlFor={id}>
+        <span>{labelText}</span>
+      </label>
+      <input
+        class="input border border-black rounded-t-lg"
+        type={type}
+        id={id}
+        name={name ?? id}
+        placeholder={placeholder}
+        required={required}
+      />
+    </div>
+  );
+}
+
+export default Standard;

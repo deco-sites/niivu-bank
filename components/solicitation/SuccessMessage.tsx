58ef4d208da66b581985955250f8d3ab35187c4c
Gabriel Matos Boubee <108771428+gabrielMatosBoubee@users.noreply.github.com>
2024-03-28 21:49:10 -0300

    feat: add other image in SuccessMessage

--- components/solicitation/SuccessMessage.tsx
+++ components/solicitation/SuccessMessage.tsx
@@ -32,21 +32,33 @@ function SucessMessage(
 ) {
   return (
     <>
-      <div class="relative w-full">
-        <div class="container flex gap-4 h-full text-white py-12">
+      <div class="relative w-full mb-8">
+        <div class="container flex gap-4 h-full text-white py-12 max-sm:px-4">
           <div class="flex flex-col gap-8">
             <span class="text-xl font-bold md:text-5xl">
               Sua solicitação foi enviada!
             </span>
-            <span class="text-base font-bold md:text-xl">
-              O número da sua solicitação é {solicitation_id}
-            </span>
+            <div class="flex items-start gap-4">
+              <span class="text-base font-bold md:text-xl">
+                O número da sua solicitação é {solicitation_id}
+              </span>
+              {icon && (
+                <Image
+                  class="sm:hidden"
+                  src={icon}
+                  width={48}
+                  height={48}
+                />
+              )}
+            </div>
             <span class="text-xs md:text-base">
               Acompanhe sua solicitação clicando no botão abaixo ou aguarde que
               enviaremos um link.
             </span>
           </div>
-          {icon && <Image src={icon} width={174} height={169} />}
+          {icon && (
+            <Image class="max-sm:hidden" src={icon} width={174} height={169} />
+          )}
         </div>
 
         <Picture preload class="absolute inset-0 -z-[1] h-full w-full">
@@ -71,7 +83,7 @@ function SucessMessage(
       </div>
       {buttonLink && (
         <div class="container">
-          <a class="btn bg-primary text-white" href={buttonLink}>
+          <a class="btn bg-primary max-sm:w-full text-white" href={buttonLink}>
             Acompanhar minha solicitação
           </a>
         </div>

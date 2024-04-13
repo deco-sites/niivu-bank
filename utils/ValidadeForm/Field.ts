40199f8326259a1d99c2762969d49a086f96862f
Jonas Jesus <jonasdasilvajesus@outlook.com>
2024-04-13 13:14:11 -0300

    fix: Refactor validateField function to improve readability and add JSDoc comments

--- utils/ValidadeForm/Field.ts
+++ utils/ValidadeForm/Field.ts
@@ -1,8 +1,8 @@
 /**
- * Verifica se o valor de uma string atende ao comprimento mínimo especificado.
- * @param {string | undefined} value - O valor a ser validado.
- * @param {number} minLength - O comprimento mínimo esperado para o valor.
- * @returns {boolean} Retorna true se o valor atender ao comprimento mínimo especificado, caso contrário retorna false.
+ * Checks if the value of a string meets the specified minimum length.
+ * @param {string | undefined} value - The value to be validated.
+ * @param {number} minLength - The minimum expected length for the value.
+ * @returns {boolean} Returns true if the value meets the specified minimum length, otherwise returns false.
  */
 export default function validateField(
   value: string | undefined,

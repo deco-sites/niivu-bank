07e40a1f658b7aebc84c20556f927f302fc51a5d
JonasJesus <jonasjesus@carcara.tech>
2024-05-28 20:39:03 -0300

    chore: changing title and order icon

--- components/ui/Card.tsx
+++ components/ui/Card.tsx
@@ -11,10 +11,10 @@ interface Props {
 export default function Card({ title, id, description }: Props) {
   return (
     <div class="w-full min-h-64 bg-white border border-[##E0E0E0] shadow-lg rounded-2xl md:rounded p-8 space-y-2">
+      <Icon size={32} id={id} />
       <h1 class="text-base font-bold truncate">
         {title}
       </h1>
-      <Icon size={32} id={id} />
       <p class="h-full text-sm font-normal">
         {description}
       </p>
